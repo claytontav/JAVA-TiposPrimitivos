9b55d21d67372242f9ce2fc6cbecaca07ec12ebb
Clayton Tavares <54451974+claytongit@users.noreply.github.com>
2020-01-27 19:56:01 -0300

    Add files via upload

---
+++ TiposPrimitivos/TiposPrimitivos.java
@@ -0,0 +1,20 @@
+class TiposPrimitivos{
+    public static void main(String[] args){
+         
+        int idade = 26;
+        double valor = 2.90;
+        float valor02 = 3.95f;
+        String nome = "Clayton";
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char o = 'o';
+
+        System.out.println("Sua idade é: " + idade);
+        System.out.println("O primeiro valor é " + valor);
+        System.out.println("O segundo valor é: " + valor02);
+        System.out.println("Seu nome é: " + nome);
+        System.out.println("O valor verdadeiro é: " + verdadeiro);
+        System.out.println("O valor de falso é: " + falso);
+        System.out.println("O valor do caracter é: " + o);
+    }
+}
